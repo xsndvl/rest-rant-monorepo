70b604e743b60ccda2bb0a35e01cf2924141ea44
Joswag Sandoval <sergiojr100@outlook.com>
2022-05-07 01:59:11 -0500

    using jwt

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require("express").Router()
 const db = require("../models")
 const bcrypt = require("bcrypt")
+const jwt = require("jwt")
 
 const { User } = db
 
@@ -14,9 +15,32 @@ router.post("/", async(req,res) => {
             message: `Could not find a user with the provided username and password`
         })
     } else {
-        res.json({ user })
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
+        res.json({ user, token: result.value })
     }
     
+})
+
+router.get("/profile", async(req, res) => {
+    try{
+
+        const [ authenticationMethod, token ] = req.headers.authorization.split(" ")
+
+        if (authenticationMethod == "Bearer") {
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+        }
+
+        const { id } = result.value
+        
+        let user = await User.findOne({
+            where: {
+                userId: req.body.userId
+            }
+        })
+        res.json(user)
+    } catch (err) {
+        res.json(err)
+    }
 })
 
 module.exports = router
